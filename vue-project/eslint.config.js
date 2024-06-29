828156913c08e2210bfef096fd6ad41f1815a2b2
biym <1291699748@qq.com>
2024-06-29 11:33:17 +0800

    first commit

---
+++ vue-project/eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...pluginVue.configs["flat/essential"],
+  {rules: {
+    indent: ['error', 4], // 用于指定代码缩进的方式，这里配置为使用四个空格进行缩进。
+   'linebreak-style': [0, 'error', 'windows'], // 用于指定换行符的风格，这里配置为使用 Windows 风格的换行符（\r\n）。
+   quotes: ['error', 'single'], // 用于指定字符串的引号风格，这里配置为使用单引号作为字符串的引号。
+   semi: ['error', 'always'], //用于指定是否需要在语句末尾添加分号，这里配置为必须始终添加分号。
+   '@typescript-eslint/no-explicit-any': ['off'] // 用于配置 TypeScript 中的 "any" 类型的使用规则，这里配置为关闭禁止显式使用 "any" 类型的检查。
+    }
+  }
+];
